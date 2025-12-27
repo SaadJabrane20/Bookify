59aa3918159333ec13bc3efeea74fb47c4feb64c
SaadJabrane20 <sjabrane20@gmail.com>
2025-12-27 21:26:44 +0100

    added some admin dashboard customization

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Booking
 # Register your models here.
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "customer",
+        "provider",
+        "service",
+        "status",
+        "start_time",
+        "end_time"
+    )
+
+    list_filter = ("status","start_time", "end_time", "provider")
+    search_fields = ("customer__username", "provider__username")
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- providers/admin.py
+++ providers/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import WorkingHours
 # Register your models here.
-admin.site.register(WorkingHours)
+@admin.register(WorkingHours)
+class WorkingHoursAdmin(admin.ModelAdmin):
+    list_display = (
+        "provider",
+        "day_of_week",
+        "start_time",
+        "end_time",
+    )
+    list_filter = ("provider", "day_of_week")
