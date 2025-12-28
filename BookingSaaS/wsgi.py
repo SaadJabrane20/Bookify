f06a26a4fc5bea6072f4cb0f3c22e3138bc32faa
SaadJabrane20 <sjabrane20@gmail.com>
2025-12-29 00:56:54 +0100

    installed gunicorn for deployment

--- BookingSaaS/wsgi.py
+++ BookingSaaS/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+path = '/home/saadjbr/Bookify'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bookify.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookingSaaS.settings')
-
 application = get_wsgi_application()
